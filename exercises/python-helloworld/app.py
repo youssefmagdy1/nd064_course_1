6482e12442b7d90840b9c3e5fba722aa0238cc37
Youusef Magdy <youssef.magdy.775@gmail.com>
2022-12-01 10:54:01 +0200

    first and second task in the project

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,34 @@
+import json
 from flask import Flask
+import logging
 app = Flask(__name__)
 
+@app.route('/status')
+def status():
+  response = app.response_class(
+          response=json.dumps({"result":"OK - healthy"}),
+          status=200,
+          mimetype='application/json'
+  )
+  return response
+
+@app.route('/metrics')
+def metrics():
+  response = app.response_class(
+          response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+          status=200,
+          mimetype='application/json'
+  )
+  return response
+
+ 
 @app.route("/")
 def hello():
-    return "Hello World!"
+  # Logging a CUSTOM message 
+  app.logger.info('Main request successfull')
+  return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+  # Stream logs to a file, and set the default log level to DEBUG
+  logging.basicConfig(filename='app.log',level=logging.INFO)
+  app.run(host='0.0.0.0')
--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,7 +1,14 @@
 import sqlite3
+import logging
+from datetime import datetime
+
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
@@ -29,6 +36,31 @@ def index():
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
     return render_template('index.html', posts=posts)
+    
+
+@app.route('/healthz')
+def healthz():
+  response = app.response_class(
+          response=json.dumps({"result":"OK - healthy"}),
+          status=200,
+          mimetype='application/json'
+  )
+  return response
+  
+
+@app.route('/metrics')
+def metrics():
+
+  connection = get_db_connection()
+  post_cnt = connection.execute('SELECT COUNT(id)  AS cnt FROM posts').fetchone()
+  connection.close()
+  response = app.response_class(
+          response=json.dumps({"db_connection_count": 1, "post_count": post_cnt['cnt']}),
+          status=200,
+          mimetype='application/json'
+  )
+  return response
+
 
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
@@ -36,13 +68,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info("["+ dt_string +"]"+ " Article "+ post['title'] +" not found ") 
       return render_template('404.html'), 404
     else:
+      app.logger.info("["+ dt_string +"]"+ "Article "+ post['title'] +" retrieved")      
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info("["+ dt_string +"]"+  "The about us page was retrieved")      
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -55,6 +90,7 @@ def create():
         if not title:
             flash('Title is required!')
         else:
+            app.logger.info( "["+ dt_string +"]"+"a new article " + title + " has been created ")      
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
@@ -67,4 +103,6 @@ def create():
 
 # start the application on port 3111
 if __name__ == "__main__":
+   logging.basicConfig(level=logging.DEBUG , datefmt='%Y-%m-%d %H:%M:%S')
+
    app.run(host='0.0.0.0', port='3111')
